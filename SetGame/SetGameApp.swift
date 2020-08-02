e259c97335d824e6450e36a6f7afcc63ed8b1fbd
David Burghoff <burghoffdavid@gmail.com>
2020-08-02 16:39:27 +0200

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by David Burghoff on 02.08.20.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
